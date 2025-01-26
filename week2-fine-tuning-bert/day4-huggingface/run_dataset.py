aefdb03247e6b15ddb5565087d2dbfb490813d1c
Zhanming <zcui@akamai.com>
2025-01-26 10:17:04 +0000

    Update day18 note

--- week2-fine-tuning-bert/day4-huggingface/run_dataset.py
+++ week2-fine-tuning-bert/day4-huggingface/run_dataset.py
@@ -3,12 +3,12 @@ from datasets import load_dataset, load_from_disk
 # Load remote dataset from Hugging Face
 remote_dataset = load_dataset(
     # data_dir="",
-    path="NousResearch/hermes-function-calling-v1",
+    path="LooksJuicy/ruozhiba",
     # cache_dir="~/.cache/huggingface/datasets",
-    cache_dir="../../remote_datasets",
-    split="train"
+    cache_dir="../../remote_datasets"
 )
 print(remote_dataset)
+
 
 # Load local cached dataset from Hugging Face
 local_dataset = load_from_disk(r"../../local_datasets/ChnSentiCorp")
@@ -20,6 +20,6 @@ for data in test_data:
     print(data)
 
 # Load csv files from local disk
-file_path = r"/day4-hugging-face/local_datasets/hermes-function-calling-v1.csv"
+file_path = r"local_datasets/hermes-function-calling-v1.csv"
 local_dataset_csv = load_dataset(path="csv", data_files=file_path)
 
