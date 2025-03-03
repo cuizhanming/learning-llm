ca4f7f0ffa8ff285567024982ca03429ba6bee4f
Zhanming <zcui@akamai.com>
2025-03-03 15:03:35 +0000

    Update with a small agent-with-smolagents project

--- applications/huggingface/agent-with-smolagents/app.py
+++ applications/huggingface/agent-with-smolagents/app.py
@@ -55,7 +55,7 @@ with open("prompts.yaml", 'r') as stream:
     
 agent = CodeAgent(
     model=model,
-    tools=[get_current_time_in_timezone, image_generation_tool, final_answer], ## add your tools here (don't remove final answer)
+    tools=[get_current_time_in_timezone, image_generation_tool, final_answer, DuckDuckGoSearchTool()], ## add your tools here (don't remove final answer)
     max_steps=6,
     verbosity_level=1,
     grammar=None,
